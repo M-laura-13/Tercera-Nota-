518d29f57e57f1b39bab54068ab24f0724b09659
Laura <mlcontreras@ufpso.edu.co>
2025-08-21 21:08:25 -0500

    primera semana

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+
 public class App {
+ 
+    public static class BankAccount {
+        //Atributos 
+    String accountNumber;
+    String owner;
+    double balance;
+    //Metodos 
+    void deposit(double amount){
+        balance +=amount;
+        System.out.println("Deposito:"+ amount + "nuevo balance:"+ balance);
+
+    }
+}
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BankAccount account1 = new BankAccount();
+        account1.accountNumber = "109-2733-134";
+        account1.balance= 2000000;
+        account1.owner = "Laura";
+        
+       account1.deposit(5000000);
+
+        System.err.println(account1.owner);
+        
+        System.err.println(account1.accountNumber);
     }
 }
